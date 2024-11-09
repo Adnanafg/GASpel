67657f9e984482940ea0b285c75552d0d545297c
Adnanafg <adnanafg3@gmail.com>
2024-11-09 21:02:16 +0100

    949

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,12 +1,21 @@
 extends Control
+@onready var sensitivty: HScrollBar = $MarginContainer/VBoxContainer/Sensitivty
+@onready var line_edit: LineEdit = $MarginContainer/VBoxContainer/LineEdit
 
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _ready() -> void:
+	line_edit.text = str(sensitivty.value)
 
 
-func _on_sensitivty_text_submitted(new_text: float) -> void:
-	Settings.mouse_sensitivty = new_text
+func _on_sensitivty_value_changed(value: float) -> void:
+	Settings.mouse_sensitivty = value
 	print(Settings.mouse_sensitivty)
+	
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	var new_value = float(new_text)
+	sensitivty.value = new_value
+	
 	
 	
 func _on_full_screen_toggled(toggled_on: bool) -> void:
@@ -14,3 +23,7 @@ func _on_full_screen_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
